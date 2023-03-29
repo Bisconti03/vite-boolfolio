772adaef87e9c69beb5559a02c45c1c550cc7349
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-03-29 15:22:57 +0200

    Milestone 4 completata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,8 @@
  
 <script>
+
+import axios from 'axios';
+
 export default {
     
     data() {
@@ -7,7 +10,15 @@ export default {
            
         };
     },
-    
+    created() {
+        axios
+        .get('http://127.0.0.1:8000/api/projects')
+        .then(response=> {
+            console.log(response.data);
+
+        });
+
+    }
 };
 </script>
 
