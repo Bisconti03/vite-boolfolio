b90e6843066c9997228ac54a6da0e47fe96ba9d6
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-03-29 15:18:12 +0200

    Milestone 3 completata

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+ 
+<script>
+export default {
+    
+    data() {
+        return {
+           
+        };
+    },
+    
+};
+</script>
+
+<template>
+   <main>
+    <h1>
+        Boolfolio
+    </h1>
+   </main>
+</template>
+
+<style scoped>
+
+</style>
